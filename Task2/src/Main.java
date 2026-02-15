1ff1452b395b2ffed1bb6b020aaa6c01f20b049f
SeNpAi2407 <160313531+SeNpAi2407@users.noreply.github.com>
2026-02-15 19:12:16 +0100

    Implement full name validation and output
    Validate input as full name and print components.

--- Task2/src/Main.java
+++ Task2/src/Main.java
@@ -1,5 +1,40 @@
+import java.util.Scanner;
+
 public class Main {
+    public static String CORRECT_CHARS = " абвгдеёжзийклмнопрстуфхцчшщъыьэюя-";
+
     public static void main(String[] args) {
-        //todo код писать тут. По желанию(рекомендуется) вынести код в методы.
+
+        String input = new Scanner(System.in).nextLine().trim().toLowerCase();
+        boolean isValid = true;
+        int spaceCount = 0;
+
+        for (int i = 0; i < input.length(); i++) {
+
+            if (input.charAt(i) == ' ') {
+                spaceCount++;
+            }
+
+            if (!CORRECT_CHARS.contains(String.valueOf(input.charAt(i)))) {
+                isValid = false;
+                break;
+            }
+        }
+
+        if (isValid && spaceCount == 2) {
+
+            String[] parts = input.split(" ");
+
+            System.out.println("Фамилия: " + capitalize(parts[0]));
+            System.out.println("Имя: " + capitalize(parts[1]));
+            System.out.println("Отчество: " + capitalize(parts[2]));
+
+        } else {
+            System.out.println("Введенная строка не является ФИО");
+        }
+    }
+
+    public static String capitalize(String word) {
+        return word.substring(0, 1).toUpperCase() + word.substring(1);
     }
 }
